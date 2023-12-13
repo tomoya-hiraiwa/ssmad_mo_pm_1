a1f49e5665fa0375fd2b0b07e732b823cff52c2b
tomoya-hiraiwa <tomoya5568.n.tfamily7667@gmail.com>
2023-12-13 11:25:30 +0900

    first commit

---
+++ ssmad_MO_pm_sample1/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ssmad_MO_pm_sample1"
+include(":app")
